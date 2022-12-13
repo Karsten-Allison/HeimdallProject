813221d0bd1a9b4327394fd0838a6eded975390f
Karsten <karstenallison@gmail.com>
2022-12-13 15:15:14 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HeimdallSwaggerTest
+# HeimdallProject
---
+++ heimdall_web_api/Models/UpdateWorkInstructionListRequest.cs
@@ -0,0 +1,11 @@
+﻿using heimdall_web_api.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace heimdall_web_api.Models
+{
+    public class UpdateWorkInstructionListRequest
+    {
+        public List<string> InstructionList { get; set; }
+    }
+}
